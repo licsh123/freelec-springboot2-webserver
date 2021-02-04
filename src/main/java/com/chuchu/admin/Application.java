6920be9ed9444692be1504d716fde3dd9d759fda
seungnhoon <licsh123@naver.com>
2021-02-04 10:19:54 +0900

    2021-02-04

--- src/main/java/com/chuchu/admin/Application.java
+++ src/main/java/com/chuchu/admin/Application.java
@@ -2,7 +2,9 @@ package com.chuchu.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditting 활성화
 @SpringBootApplication
 
 public class Application{
--- src/main/java/com/chuchu/admin/domain/posts/Posts.java
+++ src/main/java/com/chuchu/admin/domain/posts/Posts.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
